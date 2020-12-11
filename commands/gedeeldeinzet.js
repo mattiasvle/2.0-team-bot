c1b6ad025391adf1353f0cdc20026797c2bd91e6
mattiasvle <mattias.vl1994@gmail.com>
2020-12-11 23:15:15 +0100

    up

--- commands/gedeeldeinzet.js
+++ commands/gedeeldeinzet.js
@@ -1,14 +1,14 @@
 const Discord = require('discord.js')
 module.exports = {
-    name: 'groteinzet', // The name of the command
-    description: 'Verstuurd een bericht over de gestarte grote inzet!', // The description of the command (for help text)
+    name: 'Gedeeldeinzet', // The name of the command
+    description: 'Verstuurd een bericht over de gestarte gedeelde inzet waarover in de chat hulp werd gevraagd!', // The description of the command (for help text)
     args: false, // Specified that this command doesn't need any data other than the command
     usage: '', // Help text to explain how to use the command (if it had any arguments)
     execute(message) {
       const embed = new Discord.RichEmbed()
           .setTitle("Er is een grote inzet gestart!")
-          .setDescription("Er is een grote team inzet gestart! Iedereen graag de benodigde voertuigen naar deze melding sturen.")
-          .setURL("https://meldkamerspel.com")
+          .setDescription("HELP")
+          .setURL("https://www.meldkamerspel.com/missions/115540187")
           .setColor(0xD1132F)
           .setTimestamp()
           .setFooter("MeldkamerBot","https://i.ibb.co/VNk1Qn8/logo-IMG-20200921-WA0000.jpg")
