05a15fb693213e70bc12d3a747b79bb3e5631b4c
mattiasvle <mattias.vl1994@gmail.com>
2020-12-11 18:21:31 +0100

    Update opleiding.js

--- commands/opleiding.js
+++ commands/opleiding.js
@@ -1,13 +1,12 @@
 const Discord = require('discord.js')
 module.exports = {
-    name: 'Opleidingen', // The name of the command
-    description: 'Verstuurd een bericht over de gestarte opleidingen!', // The description of the command (for help text)
+    name: 'opleiding', // The name of the command
+    description: 'Verstuurd een bericht over de gestarte opleiding!', // The description of the command (for help text)
     args: false, // Specified that this command doesn't need any data other than the command
     usage: '', // Help text to explain how to use the command (if it had any arguments)
     execute(message) {
       const embed = new Discord.RichEmbed()
- 
-          .setTitle("Er is een opleiding!")
+          .setTitle("Er is een opleiding gestart!")
           .setDescription("Er is een grote team inzet gestart! Iedereen graag de benodigde voertuigen naar deze melding sturen.")
           .setURL("https://meldkamerspel.com")
           .setColor(0xD1132F)
