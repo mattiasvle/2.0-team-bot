9177bc2c7e50dd1feb4c06518e939b72d3b0b848
mattiasvle <mattias.vl1994@gmail.com>
2020-12-12 22:41:01 +0100

    Update opleiding.js

--- commands/opleiding.js
+++ commands/opleiding.js
@@ -5,24 +5,16 @@ module.exports = {
     args: false, // Specified that this command doesn't need any data other than the command
     usage: '', // Help text to explain how to use the command (if it had any arguments)
     execute(message, args) {
-      let link = args.slice(0).join(" ");
-      if(!link) {
-        link = "Er is geen link opgegeven.";
-      }
-      let opleiding = args.slice(1).join(" ");
-      if(!opleiding) {
-        opleiding = "Er is geen opleiding opgegeven.";
-      }
-      let vrijeplaatsen = args.slice(2).join(" ");
-      if(!plaatsen) {
-        plaatsen = "Er is geen aantal plaatsen opgegeven.";
+      if (message.content.startsWith(';opleidinggestart')) {
+          let rest_of_the_string = message.content.slice(';opleidinggestart'.length); //removes the first part
+          let array_of_arguments = rest_of_the_string.split('*'); //[title, description, link, image]
       }
       const embed = new Discord.RichEmbed()
           .setTitle("Er is een inzet gedeeld voor hulp!")
           .setDescription("HELP")
-          .addField("link:",`${link}`)
-          .addField("opleiding:",`${opleiding}`)
-          .addField("plaatsen:",`${plaatsen}`)
+          .addField('Opleiding:', array_of_arguments[0])
+          .addField('link:', array_of_arguments[1])
+          .addField('plaatsen:', array_of_arguments[0])
           .setURL("https://www.meldkamerspel.com")
           .setColor(0xD1132F)
           .setTimestamp()
