f084340744b04fba65946b124735ff780e4880b6
mattiasvle <mattias.vl1994@gmail.com>
2020-12-13 10:35:06 +0100

    Update opleiding.js

--- commands/opleiding.js
+++ commands/opleiding.js
@@ -6,19 +6,20 @@ module.exports = {
     usage: '', // Help text to explain how to use the command (if it had any arguments)
     execute(message, args) {
       if (message.content.startsWith(';opleiding')) {
-          let rest_of_the_string = message.content.slice(';opleidinggestart'.length); //removes the first part
+          let rest_of_the_string = message.content.slice(';opleiding'.length); //removes the first part
           let array_of_arguments = rest_of_the_string.split('*'); //[title, description, link, image]
       }
       const embed = new Discord.RichEmbed()
           .setTitle("Er is een inzet gedeeld voor hulp!")
           .setDescription("HELP")
-          .addField('Opleiding:', array_of_arguments[0])
-          .addField('link:', array_of_arguments[1])
-          .addField('plaatsen:', array_of_arguments[2])
+          
           .setURL("https://www.meldkamerspel.com")
           .setColor(0xD1132F)
           .setTimestamp()
           .setFooter("MeldkamerBot","https://i.ibb.co/VNk1Qn8/logo-IMG-20200921-WA0000.jpg")
+          .addField('Opleiding:', array_of_arguments[0])
+          .addField('link:', array_of_arguments[1])
+          .addField('plaatsen:', array_of_arguments[2])
          
 
       message.channel.send(embed);
